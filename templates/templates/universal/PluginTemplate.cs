f28f3ef6dc87068d13d498b8bfc3f690456d6cc2
Enes Sadık Özbek <es.ozbek@outlook.com>
2020-06-27 20:33:56 +0300

    Add dotnet templates, closes #56.

---
+++ templates/templates/universal/PluginTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OpenMod.Core.Plugins;
+
+[assembly: PluginMetadata("PLUGIN-ID", DisplayName = "PLUGIN-NAME")]
+namespace MyOpenModPlugin
+{
+    public class MyOpenModPlugin : OpenModUniversalPlugin
+    {
+        private readonly ILogger<MyOpenModPlugin> m_Logger;
+
+        public MyOpenModPlugin(ILogger<MyOpenModPlugin> logger, IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_Logger = logger;
+        }
+
+        protected override Task OnLoadAsync()
+        {
+            m_Logger.LogInformation("Hello World!");
+            return Task.CompletedTask;
+        }
+
+        protected override Task OnUnloadAsync()
+        {
+            m_Logger.LogInformation("Good bye :(");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ templates/templates/unturned/PluginTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OpenMod.Core.Plugins;
+using OpenMod.Unturned.Plugins;
+
+[assembly: PluginMetadata("PLUGIN-ID", DisplayName = "PLUGIN-NAME")]
+namespace MyOpenModPlugin
+{
+    public class MyOpenModPlugin : OpenModUnturnedPlugin
+    {
+        private readonly ILogger<MyOpenModPlugin> m_Logger;
+
+        public MyOpenModPlugin(ILogger<MyOpenModPlugin> logger, IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_Logger = logger;
+        }
+
+        protected override Task OnLoadAsync()
+        {
+            m_Logger.LogInformation("Hello World!");
+            return Task.CompletedTask;
+        }
+
+        protected override Task OnUnloadAsync()
+        {
+            m_Logger.LogInformation("Good bye :(");
+            return Task.CompletedTask;
+        }
+    }
+}
