bbd3b55870bb35404d7695785417045738496e8f
Enes Sadık Özbek <es.ozbek@outlook.com>
2020-07-01 18:05:57 +0300

    Update templates

--- templates/templates/universal/PluginTemplate.cs
+++ templates/templates/universal/PluginTemplate.cs
@@ -1,29 +1,41 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using OpenMod.Core.Plugins;
+
+// For more, visit https://openmod.github.io/openmod-docs/development-guide/making-your-first-plugin/
 
 [assembly: PluginMetadata("PLUGIN-ID", DisplayName = "PLUGIN-NAME")]
 namespace MyOpenModPlugin
 {
     public class MyOpenModPlugin : OpenModUniversalPlugin
     {
+        private readonly IConfiguration m_Configuration;
+        private readonly IStringLocalizer m_StringLocalizer;
         private readonly ILogger<MyOpenModPlugin> m_Logger;
 
-        public MyOpenModPlugin(ILogger<MyOpenModPlugin> logger, IServiceProvider serviceProvider) : base(serviceProvider)
+        public MyOpenModPlugin(
+            IConfiguration configuration, 
+            IStringLocalizer stringLocalizer,
+            ILogger<MyOpenModPlugin> logger, 
+            IServiceProvider serviceProvider) : base(serviceProvider)
         {
+            m_Configuration = configuration;
+            m_StringLocalizer = stringLocalizer;
             m_Logger = logger;
         }
 
         protected override Task OnLoadAsync()
         {
-            m_Logger.LogInformation("Hello World!");
+            m_Logger.LogInformation(m_StringLocalizer["plugin_events:plugin_start"]);
             return Task.CompletedTask;
         }
 
         protected override Task OnUnloadAsync()
         {
-            m_Logger.LogInformation("Good bye :(");
+            m_Logger.LogInformation(m_StringLocalizer["plugin_events:plugin_stop"]);
             return Task.CompletedTask;
         }
     }
--- templates/templates/unturned/PluginTemplate.cs
+++ templates/templates/unturned/PluginTemplate.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Cysharp.Threading.Tasks;
 using OpenMod.Core.Plugins;
 using OpenMod.Unturned.Plugins;
+
+// For more, visit https://openmod.github.io/openmod-docs/development-guide/making-your-first-plugin/
 
 [assembly: PluginMetadata("PLUGIN-ID", DisplayName = "PLUGIN-NAME")]
 namespace MyOpenModPlugin
 {
     public class MyOpenModPlugin : OpenModUnturnedPlugin
     {
+        private readonly IConfiguration m_Configuration;
+        private readonly IStringLocalizer m_StringLocalizer;
         private readonly ILogger<MyOpenModPlugin> m_Logger;
 
-        public MyOpenModPlugin(ILogger<MyOpenModPlugin> logger, IServiceProvider serviceProvider) : base(serviceProvider)
+        public MyOpenModPlugin(
+            IConfiguration configuration, 
+            IStringLocalizer stringLocalizer,
+            ILogger<MyOpenModPlugin> logger, 
+            IServiceProvider serviceProvider) : base(serviceProvider)
         {
+            m_Configuration = configuration;
+            m_StringLocalizer = stringLocalizer;
             m_Logger = logger;
         }
 
@@ -21,14 +33,14 @@ namespace MyOpenModPlugin
         {
 			// await UniTask.SwitchToMainThread(); uncomment if you have to access Unturned or UnityEngine APIs
             m_Logger.LogInformation("Hello World!");
-            return UniTask.CompletedTask;
+			
+			// await UniTask.SwitchToThreadPool(); // you can switch back to a different thread
         }
 
         protected override async UniTask OnUnloadAsync()
         {
             // await UniTask.SwitchToMainThread(); uncomment if you have to access Unturned or UnityEngine APIs
-            m_Logger.LogInformation("Good bye :(");
-            return UniTask.CompletedTask;
+            m_Logger.LogInformation(m_StringLocalizer["plugin_events:plugin_stop"]);
         }
     }
 }
