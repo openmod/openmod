43274cc3cacdcadc0df2abfa0c0a020fbe3576d6
Enes Sadık Özbek <es.ozbek@outlook.com>
2020-07-05 12:50:10 +0300

    Remove debug logs

--- unturned/OpenMod.Unturned/Users/UnturnedUserProvider.cs
+++ unturned/OpenMod.Unturned/Users/UnturnedUserProvider.cs
@@ -305,7 +305,6 @@ namespace OpenMod.Unturned.Users
         public void Dispose()
         {
             m_UnturnedUsers.Clear();
-            System.Console.WriteLine("UnturnedUserProvider: disposing");
 
             // ReSharper disable DelegateSubtraction
             Provider.onCheckValidWithExplanation -= OnPendingPlayerConnecting;
