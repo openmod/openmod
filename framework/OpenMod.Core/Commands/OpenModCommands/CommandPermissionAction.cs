13986dcd2591da22fa13f68c1530ad3827dfb9cb
Stephen White <iamsilk3495@gmail.com>
2020-07-12 18:45:05 -0300

    Fixes 'Player not found' error
    Whenever the argument "p" is used in place of "player", "p" is passed on as the `userType` argument.

--- framework/OpenMod.Core/Commands/OpenModCommands/CommandPermissionAction.cs
+++ framework/OpenMod.Core/Commands/OpenModCommands/CommandPermissionAction.cs
@@ -54,7 +54,7 @@ namespace OpenMod.Core.Commands.OpenModCommands
                 case "player":
                     permission = "Manage.Players";
                     var id = await Context.Parameters.GetAsync<string>(1);
-                    var user = await m_UserDataStore.GetUserDataAsync(id, actorType);
+                    var user = await m_UserDataStore.GetUserDataAsync(id, "player");
 
                     if (user == null)
                     {
