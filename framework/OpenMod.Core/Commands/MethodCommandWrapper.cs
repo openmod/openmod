dbbe18db9e04f8f3333b05fe9390135914ab2c69
Andrii Vitiv <cyberandrii@gmail.com>
2020-07-24 20:50:56 +0300

    Implement method-based commands. Closes #60

--- framework/OpenMod.Core/Commands/MethodCommandWrapper.cs
+++ framework/OpenMod.Core/Commands/MethodCommandWrapper.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using OpenMod.API;
 using OpenMod.API.Commands;
+using OpenMod.Core.Helpers;
 using OpenMod.Core.Ioc;
 
 namespace OpenMod.Core.Commands
@@ -12,16 +15,20 @@ namespace OpenMod.Core.Commands
     public class MethodCommandWrapper : ICommand
     {
         private readonly MethodInfo m_MethodInfo;
+        private readonly IServiceProvider m_ServiceProvider;
 
         public MethodCommandWrapper(MethodInfo methodInfo, IServiceProvider serviceProvider)
         {
             m_MethodInfo = methodInfo;
+            m_ServiceProvider = serviceProvider;
         }
 
-        public Task ExecuteAsync()
+        public async Task ExecuteAsync()
         {
-            // todo: implement MethodCommandWrapper
-            throw new NotImplementedException();
+            IEnumerable<Type> paramTypes = m_MethodInfo.GetParameters().Select(x => x.ParameterType);
+            object[] paramValues = paramTypes.Select(x => m_ServiceProvider.GetService(x)).ToArray();
+
+            await m_MethodInfo.InvokeWithTaskSupportAsync(null, paramValues);
         }
     }
 }
