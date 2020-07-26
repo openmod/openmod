a04e1d5518af5a2d9a03e81e8b98e77acd81400b
Rúben Garcia <rube200@users.noreply.github.com>
2020-07-26 20:25:20 +0100

    Pass balance on NotEnoughBalanceExcetion

--- extensions/OpenMod.Extensions.Economy.Abstractions/NotEnoughBalanceException.cs
+++ extensions/OpenMod.Extensions.Economy.Abstractions/NotEnoughBalanceException.cs
@@ -4,8 +4,15 @@ namespace OpenMod.Extensions.Economy.Abstractions
 {
     public class NotEnoughBalanceException : UserFriendlyException
     {
+        public decimal? Balance = null;
+
         public NotEnoughBalanceException(string message) : base(message)
         {
+        }
+
+        public NotEnoughBalanceException(string message, decimal balance) : base(message)
+        {
+            Balance = balance;
         }
     }
 }
