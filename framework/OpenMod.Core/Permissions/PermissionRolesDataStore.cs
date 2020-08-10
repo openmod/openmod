114a071953308b2e1852aa9d14287b2759fc8796
Enes Sadık Özbek <es.ozbek@outlook.com>
2020-08-11 02:00:18 +0300

    Change sample permissions to new permissions format

--- framework/OpenMod.Core/Permissions/PermissionRolesDataStore.cs
+++ framework/OpenMod.Core/Permissions/PermissionRolesDataStore.cs
@@ -40,7 +40,7 @@ namespace OpenMod.Core.Permissions
                         Priority = 0,
                         Permissions = new HashSet<string> 
                         {
-                            "OpenMod.Core.commands.help"
+                            "OpenMod.Core:commands.help"
                         },
                         IsAutoAssigned = true
                     },
@@ -54,7 +54,7 @@ namespace OpenMod.Core.Permissions
                         },
                         Permissions = new HashSet<string>
                         {
-                            "SomeKitsPlugin.kits.vip"
+                            "SomeKitsPlugin:kits.vip"
                         },
                         IsAutoAssigned = false
                     }
