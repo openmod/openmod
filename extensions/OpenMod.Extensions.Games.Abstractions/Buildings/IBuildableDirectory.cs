dabecbf9cecac82d78f61fe2f93c1b3426b45ba4
Pustalorc <persiafighter@gmail.com>
2020-08-18 15:46:49 +0200

    Initial abstraction of buildables

---
+++ extensions/OpenMod.Extensions.Games.Abstractions/Buildings/IBuildableDirectory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenMod.API.Ioc;
+
+namespace OpenMod.Extensions.Games.Abstractions.Buildings
+{
+    [Service]
+    public interface IBuildableDirectory
+    {
+        Task<IReadOnlyCollection<IBuildableAsset>> GetBuildableAssetsAsync();
+
+        Task<IReadOnlyCollection<IBuildable>> GetBuildablesAsync();
+    }
+}
---
+++ extensions/OpenMod.Extensions.Games.Abstractions/Buildings/IOwnership.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using OpenMod.Extensions.Games.Abstractions.Players;
+
+namespace OpenMod.Extensions.Games.Abstractions.Buildings
+{
+    public interface IOwnership
+    {
+        string OwnerPlayerId { get; }
+
+        string AssociatedGroupId { get; }
+
+        Task<bool> HasAccess(IPlayer player);
+    }
+}
