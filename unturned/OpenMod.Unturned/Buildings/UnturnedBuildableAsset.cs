41ac6d16a71e760c8a1a6b6def5a4684cf6644de
Pustalorc <persiafighter@gmail.com>
2020-08-18 15:49:31 +0200

    Initial unturned buildable abstraction implementation

---
+++ unturned/OpenMod.Unturned/Buildings/UnturnedBuildableAsset.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using OpenMod.Extensions.Games.Abstractions.Buildings;
+using OpenMod.Extensions.Games.Abstractions.Vehicles;
+using SDG.Unturned;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using UnityEngine;
+
+namespace OpenMod.Unturned.Buildings
+{
+    public class UnturnedBuildableAsset : IBuildableAsset
+    {
+        public string BuildableAssetId { get; }
+
+        public string BuildableType { get; }
+
+        public UnturnedBuildableAsset(Asset asset)
+        {
+            BuildableAssetId = asset.id.ToString();
+            BuildableType = asset.GetType().Name;
+        }
+    }
+}
---
+++ unturned/OpenMod.Unturned/Buildings/UnturnedBuildableState.cs
@@ -0,0 +1,23 @@
+﻿using OpenMod.Extensions.Games.Abstractions.Buildings;
+using SDG.Unturned;
+
+namespace OpenMod.Unturned.Buildings
+{
+    public class UnturnedBuildableState : IBuildableState
+    {
+        public ushort Health { get; }
+
+        public byte[] State { get; }
+
+        public UnturnedBuildableState(Barricade barricade)
+        {
+            Health = barricade.health;
+            State = barricade.state;
+        }
+
+        public UnturnedBuildableState(Structure structure)
+        {
+            Health = structure.health;
+        }
+    }
+}
