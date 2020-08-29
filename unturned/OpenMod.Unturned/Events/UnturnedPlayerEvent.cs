b5d9edd1872da1d1c7b0dd541c23f915f0b1d08e
Stephen White <iamsilk3495@gmail.com>
2020-08-28 21:58:47 -0300

    Implemented game abstractions to base unturned player event
    This commit was meant to be added earlier

--- unturned/OpenMod.Unturned/Events/UnturnedPlayerEvent.cs
+++ unturned/OpenMod.Unturned/Events/UnturnedPlayerEvent.cs
@@ -1,9 +1,10 @@
 ﻿using OpenMod.Core.Eventing;
+using OpenMod.Extensions.Games.Abstractions.Players;
 using OpenMod.Unturned.Entities;
 
 namespace OpenMod.Unturned.Events
 {
-    public abstract class UnturnedPlayerEvent : Event
+    public abstract class UnturnedPlayerEvent : Event, IPlayerEvent
     {
         protected UnturnedPlayerEvent(UnturnedPlayer player)
         {
@@ -11,5 +12,7 @@ namespace OpenMod.Unturned.Events
         }
 
         public UnturnedPlayer Player { get; }
+
+        IPlayer IPlayerEvent.Player => Player;
     }
 }
