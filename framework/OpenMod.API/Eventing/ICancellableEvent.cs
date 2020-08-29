24aa53ff7a2925e3dcbf56c9cd38bc7478d0de0c
Stephen White <iamsilk3495@gmail.com>
2020-08-29 02:22:46 -0300

    Change ICancellableEvent to implement IEvent

--- framework/OpenMod.API/Eventing/ICancellableEvent.cs
+++ framework/OpenMod.API/Eventing/ICancellableEvent.cs
@@ -3,7 +3,7 @@
     /// <summary>
     ///     Defines an event that can be cancelled.
     /// </summary>
-    public interface ICancellableEvent
+    public interface ICancellableEvent : IEvent
     {
         /// <summary>
         ///     Sets if the event action should be cancelled.
