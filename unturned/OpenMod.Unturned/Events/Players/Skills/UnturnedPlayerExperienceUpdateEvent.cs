2e1a536a627ec1f96e95d8323ab9ed3c7df92440
Stephen White <iamsilk3495@gmail.com>
2020-08-30 14:02:35 -0300

    Add experience update event

---
+++ unturned/OpenMod.Unturned/Events/Players/Skills/UnturnedPlayerExperienceUpdateEvent.cs
@@ -0,0 +1,14 @@
+﻿using OpenMod.Unturned.Entities;
+
+namespace OpenMod.Unturned.Events.Players.Skills
+{
+    public class UnturnedPlayerExperienceUpdateEvent : UnturnedPlayerEvent
+    {
+        public uint Experience { get; }
+
+        public UnturnedPlayerExperienceUpdateEvent(UnturnedPlayer player, uint experience) : base(player)
+        {
+            Experience = experience;
+        }
+    }
+}
