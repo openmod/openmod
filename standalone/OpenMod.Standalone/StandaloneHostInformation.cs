b21c1b30be12f34dc51b0c15c67059d8726c030a
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-09-14 19:57:26 +0700

    Add Standalone host information

---
+++ standalone/OpenMod.Standalone/StandaloneHostInformation.cs
@@ -0,0 +1,17 @@
+﻿using OpenMod.API;
+using OpenMod.Core.Helpers;
+using Semver;
+
+namespace OpenMod.Standalone
+{
+    public class StandaloneHostInformation : IHostInformation
+    {
+        public StandaloneHostInformation()
+        {
+            HostVersion = VersionHelper.ParseAssemblyVersion(GetType().Assembly);
+        }
+        public SemVersion HostVersion { get; }
+
+        public string HostName => "Openmod Standalone";
+    }
+}
