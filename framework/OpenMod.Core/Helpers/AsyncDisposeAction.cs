ba82595956ba94033eafc2383f393ddf6f43622e
Enes Sadık Özbek <es.ozbek@outlook.com>
2020-09-16 23:33:03 +0300

    Add AsyncDisposable

---
+++ framework/OpenMod.Core/Helpers/AsyncDisposeAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OpenMod.Core.Helpers
+{
+    public sealed class AsyncDisposeAction : IAsyncDisposable
+    {
+        private readonly Func<Task> m_DisposeTask;
+
+        public AsyncDisposeAction(Func<Task> disposeTask)
+        {
+            m_DisposeTask = disposeTask;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (m_DisposeTask == null)
+            {
+                return;
+            }
+
+            await m_DisposeTask.Invoke();
+        }
+    }
+}
