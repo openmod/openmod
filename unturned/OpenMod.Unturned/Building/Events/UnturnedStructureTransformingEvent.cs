81bad6e7c678a784c636c9c026f422f9867a8e2a
Vicente Pastor <persiafighter@gmail.com>
2020-10-12 01:16:37 +0100

    Added Transform related Events
    Fixed a missing unsubscriptions from events from Unsubscribe (destroy events)
    Added all related events to buildable transforming and their related hooks.
    Added 2 PostFix patches to support post-transformed.

---
+++ unturned/OpenMod.Unturned/Building/Events/UnturnedStructureTransformingEvent.cs
@@ -0,0 +1,17 @@
+﻿using OpenMod.Unturned.Players;
+using Steamworks;
+using UnityEngine;
+
+namespace OpenMod.Unturned.Building.Events
+{
+    public class UnturnedStructureTransformingEvent : UnturnedBuildableTransformingEvent, IUnturnedStructureEvent
+    {
+        public new UnturnedStructureBuildable Buildable => (UnturnedStructureBuildable) base.Buildable;
+
+        public UnturnedStructureTransformingEvent(UnturnedStructureBuildable buildable, UnturnedPlayer instigator,
+            CSteamID instigatorId, Vector3 point, Quaternion rotation) : base(buildable, instigator, instigatorId,
+            point, rotation)
+        {
+        }
+    }
+}
