7930a0540d240f8a6aa0d64831f5300d97ec8afd
Rúben Garcia <rube200@users.noreply.github.com>
2020-10-14 03:43:18 +0100

    Patchs for shutdown

---
+++ unturned/OpenMod.Unturned.Module.Shared/Patches/UnturnedBroadcastShutdownPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using SDG.Unturned;
+
+namespace OpenMod.Unturned.Module.Shared.Patches
+{
+    [HarmonyPatch(typeof(Provider))]
+    [HarmonyPatch("broadcastServerShutdown")]
+    public static class UnturnedBroadcastShutdownPatch
+    {
+        [HarmonyPostfix]
+        public static void AfterBroadcastShutdown()
+        {
+            SaveManager.save();
+        }
+    }
+}
