9e71f574a9c7bec97ba163f4d1758f758179303f
Andrii Vitiv <cyberandrii@gmail.com>
2020-10-30 21:22:12 +0200

    Add ICommandParameterResolveProvider

---
+++ framework/OpenMod.API/Commands/ICommandParameterResolveProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace OpenMod.API.Commands
+{
+    public interface ICommandParameterResolveProvider
+    {
+        /// <summary>
+        /// Determines whether the given type is supported by this resolver.
+        /// </summary>
+        bool Supports([NotNull] Type type);
+
+        /// <summary>
+        /// Resolves an object of the given type from the input.
+        /// </summary>
+        [ItemCanBeNull]
+        Task<object> ResolveAsync([NotNull] Type type, [NotNull] string input);
+    }
+}
