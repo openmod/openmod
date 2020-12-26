523b597a5e3ec246509ea7b14b0dc3f95d0bc95f
Andrii Vitiv <cyberandrii@gmail.com>
2020-12-27 00:28:43 +0200

    Use wrapper types for items in events

--- rust/OpenMod.Rust/Players/Inventory/Events/RustPlayerPickingUpItemEvent.cs
+++ rust/OpenMod.Rust/Players/Inventory/Events/RustPlayerPickingUpItemEvent.cs
@@ -1,17 +1,18 @@
 ﻿using OpenMod.API.Eventing;
+using OpenMod.Rust.Items;
 using OpenMod.Rust.Players.Events;
 
 namespace OpenMod.Rust.Players.Inventory.Events
 {
     public class RustPlayerPickingUpItemEvent : RustPlayerEvent, ICancellableEvent
     {
-        public Item Item { get; }
         public int TargetPos { get; }
+        public RustItem Item { get; }
         public bool IsCancelled { get; set; }
 
         public RustPlayerPickingUpItemEvent(
             RustPlayer player,
-            Item item, 
+            RustItem item,
             int targetPos) : base(player)
         {
             Item = item;
