7829d807f58208d586629d902cbfc73266335be9
Enes Sadık Özbek <es.ozbek@outlook.com>
2021-01-14 04:46:00 +0300

    Add conditional patching for harmony

---
+++ framework/OpenMod.Core/Patching/HarmonyConditionalPatchAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace OpenMod.Core.Patching
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Delegate | AttributeTargets.Method, AllowMultiple = true)]
+    public class HarmonyConditionalPatchAttribute : Attribute
+    {
+        public string Condition { get; }
+
+        public HarmonyConditionalPatchAttribute(string condition)
+        {
+            Condition = condition;
+        }
+    }
+}
---
+++ framework/OpenMod.Core/Patching/HarmonyExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+
+namespace OpenMod.Core.Patching
+{
+    public static class HarmonyExtensions
+    {
+        public static void PatchAllConditional(this Harmony harmony, Assembly asm, params string[] conditions)
+        {
+            var conditionsSet = new HashSet<string>(conditions, StringComparer.OrdinalIgnoreCase);
+
+            AccessTools.GetTypesFromAssembly(asm)
+                .Do(type =>
+                {
+                    var typeConditions = type.GetCustomAttributes<HarmonyConditionalPatchAttribute>()
+                        .Select(d => d.Condition)
+                        .ToList();
+
+                    if (!typeConditions.Any() || !typeConditions.Any(d => conditionsSet.Contains(d)))
+                    {
+                        return;
+                    }
+
+                    harmony.CreateClassProcessor(type).Patch();
+                });
+        }
+    }
+}
