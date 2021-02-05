63f3f2050c67ef49d17dec38e4554ad6b7da8683
Enes Sadık Özbek <es.ozbek@outlook.com>
2021-02-05 03:12:32 +0300

    Fix analyzer package not working

---
+++ framework/OpenMod.Analyzers/tools/uninstall.ps1
@@ -0,0 +1,7 @@
+﻿foreach ($analyzerFilePath in Get-ChildItem $analyzersPath -Filter *.dll)
+{
+        if($project.Object.AnalyzerReferences)
+        {
+                $project.Object.AnalyzerReferences.Add($analyzerFilePath.FullName)
+        }
+}
