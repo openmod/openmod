df52ffa77471b421072f335e25ed10d9765b460f
Stephen White <iamsilk3495@gmail.com>
2021-02-28 16:50:45 -0400

    Added attribute-based permission registrations
    Added for OpenMod.Core, OpenMod's host, and all loaded plugins.

---
+++ framework/OpenMod.Core/Permissions/ComponentLoadedEventListener.cs
@@ -0,0 +1,58 @@
+﻿using JetBrains.Annotations;
+using OpenMod.API;
+using OpenMod.API.Eventing;
+using OpenMod.API.Permissions;
+using OpenMod.Core.Events;
+using OpenMod.Core.Plugins.Events;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace OpenMod.Core.Permissions
+{
+    [OpenModInternal]
+    [UsedImplicitly]
+    internal class ComponentLoadedEventListener :
+        IEventListener<OpenModInitializedEvent>,
+        IEventListener<PluginLoadedEvent>
+    {
+        private readonly IRuntime m_Runtime;
+        private readonly IPermissionRegistry m_PermissionRegistry;
+
+        public ComponentLoadedEventListener(IRuntime runtime,
+            IPermissionRegistry permissionRegistry)
+        {
+            m_Runtime = runtime;
+            m_PermissionRegistry = permissionRegistry;
+        }
+
+        private void RegisterComponentPermissions(IOpenModComponent component, Assembly? assembly = null)
+        {
+            assembly ??= component.GetType().Assembly;
+
+            var attribs = assembly.GetCustomAttributes<RegisterPermissionAttribute>();
+
+            foreach (var attrib in attribs)
+            {
+                m_PermissionRegistry.RegisterPermission(component, attrib.Permission, attrib.Description,
+                    attrib.DefaultGrant);
+            }
+        }
+
+        public Task HandleEventAsync(object? sender, OpenModInitializedEvent @event)
+        {
+            // OpenMod.Core
+            RegisterComponentPermissions(m_Runtime, GetType().Assembly);
+            
+            RegisterComponentPermissions(@event.Host);
+
+            return Task.CompletedTask;
+        }
+
+        public Task HandleEventAsync(object? sender, PluginLoadedEvent @event)
+        {
+            RegisterComponentPermissions(@event.Plugin);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ framework/OpenMod.Core/Permissions/RegisterPermissionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using OpenMod.API.Permissions;
+using System;
+
+namespace OpenMod.Core.Permissions
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true, Inherited = true)]
+    public class RegisterPermissionAttribute : Attribute
+    {
+        public string Permission { get; }
+
+        public string? Description { get; set; }
+
+        public PermissionGrantResult? DefaultGrant { get; set; }
+
+        public RegisterPermissionAttribute(string permission)
+        {
+            Permission = permission;
+        }
+    }
+}
