3b88b0cc2a0e227f0aec296fa99e17054a48cbcf
Stephen White <iamsilk3495@gmail.com>
2021-02-28 18:18:30 -0400

    Added cooldown persistence

---
+++ framework/OpenMod.Core/Cooldowns/CooldownRecords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenMod.Core.Cooldowns
+{
+    [Serializable]
+    public class CooldownRecords
+    {
+        public Dictionary<string, List<CooldownRecord>> Records { get; set; }
+
+        public CooldownRecords()
+        {
+            Records = new Dictionary<string, List<CooldownRecord>>();
+        }
+
+        public CooldownRecords(Dictionary<string, List<CooldownRecord>> records)
+        {
+            Records = records;
+        }
+    }
+}
