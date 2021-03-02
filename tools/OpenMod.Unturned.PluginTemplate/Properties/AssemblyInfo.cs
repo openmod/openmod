eb97e15e113d96fbcee19164d64deb1ffe13465b
Stephen White <iamsilk3495@gmail.com>
2021-03-02 01:03:12 -0400

    Added OpenMod.Unturned plugin template for Microsoft VS

---
+++ tools/OpenMod.Unturned.PluginTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenMod.Unturned.PluginTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OpenMod")]
+[assembly: AssemblyProduct("OpenMod.Unturned.PluginTemplate")]
+[assembly: AssemblyCopyright("Copyright © OpenMod 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22ce5f4e-c95c-4b98-afd7-e1108fa35cbc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tools/OpenMod.Unturned.PluginWizard/WizardImplementation.cs
@@ -0,0 +1,65 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+// ReSharper disable StringLiteralTypo
+
+namespace OpenMod.Unturned.PluginWizard
+{
+    public class WizardImplementation : IWizard
+    {
+        // This method is called before opening any item that
+        // has the OpenInEditor attribute.
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public void ProjectItemFinishedGenerating(ProjectItem
+            projectItem)
+        {
+        }
+
+        // This method is called after the project is created.
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                // Display a form to the user. The form collects
+                // input for the custom message.
+                var inputWindow = new UserInputWindow(replacementsDictionary);
+
+                //inputWindow.AddInput("$displayname$", "Display Name:", "Unturned Plugin");
+                //inputWindow.AddInput("$description$", "Description:", "An OpenMod Unturned plugin.");
+                //inputWindow.AddInput("$author$", "Author:", "Your name");
+
+                inputWindow.ShowDialog();
+
+                inputWindow.ApplyInputs(replacementsDictionary);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
