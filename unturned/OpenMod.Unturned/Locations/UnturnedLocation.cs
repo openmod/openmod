8e7c46f69357ce276a09864cec19d4c96beec1f9
Stephen White <iamsilk3495@gmail.com>
2021-03-16 20:54:08 -0300

    Added Unturned location wrappers

---
+++ unturned/OpenMod.Unturned/Locations/UnturnedLocation.cs
@@ -0,0 +1,20 @@
+﻿using OpenMod.UnityEngine.Extensions;
+using SDG.Unturned;
+using System.Numerics;
+
+namespace OpenMod.Unturned.Locations
+{
+    public class UnturnedLocation
+    {
+        public UnturnedLocation(LocationNode node)
+        {
+            Node = node;
+        }
+
+        public string Name => Node.name;
+
+        public Vector3 Position => Node.point.ToSystemVector();
+
+        public LocationNode Node { get; }
+    }
+}
