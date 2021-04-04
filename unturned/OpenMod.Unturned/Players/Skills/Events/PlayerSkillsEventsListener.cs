40cfc6b5e3485ce96256077a1f960712f4cbe088
Feli <felinegri5@gmail.com>
2021-04-03 23:34:07 -0300

    Add Voice Events and Reputation Event

--- unturned/OpenMod.Unturned/Players/Skills/Events/PlayerSkillsEventsListener.cs
+++ unturned/OpenMod.Unturned/Players/Skills/Events/PlayerSkillsEventsListener.cs
@@ -18,11 +18,22 @@ namespace OpenMod.Unturned.Players.Skills.Events
         public override void Subscribe()
         {
             PlayerSkills.OnExperienceChanged_Global += PlayerSkills_OnExperienceChanged_Global;
+            PlayerSkills.OnReputationChanged_Global += PlayerSkills_OnReputationChanged_Global;
+        }
+
+        private void PlayerSkills_OnReputationChanged_Global(PlayerSkills skills, int reputation)
+        {
+            var player = GetUnturnedPlayer(skills.player)!;
+
+            var @event = new UnturnedPlayerReputationUpdatedEvent(player, reputation);
+
+            Emit(@event);
         }
 
         public override void Unsubscribe()
         {
             PlayerSkills.OnExperienceChanged_Global -= PlayerSkills_OnExperienceChanged_Global;
+            PlayerSkills.OnReputationChanged_Global -= PlayerSkills_OnReputationChanged_Global;
         }
 
         private void PlayerSkills_OnExperienceChanged_Global(PlayerSkills skills, uint experience)
