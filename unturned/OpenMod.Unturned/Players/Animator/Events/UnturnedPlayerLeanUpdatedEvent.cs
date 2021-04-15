fc128fd0f1838567fced09dd21b798cb90d53083
Feli <felinegri5@gmail.com>
2021-04-15 20:51:25 -0300

    Add UnturnedPlayerLeanUpdatedEvent

---
+++ unturned/OpenMod.Unturned/Players/Animator/Events/UnturnedPlayerLeanUpdatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using OpenMod.Unturned.Events;
+
+namespace OpenMod.Unturned.Players.Animator.Events
+{
+    public class UnturnedPlayerLeanUpdatedEvent : UnturnedPlayerEvent
+    {
+        public LeanType Lean { get; }
+        public UnturnedPlayerLeanUpdatedEvent(UnturnedPlayer player, LeanType lean) : base(player)
+        {
+            Lean = lean;
+        }
+    }
+}
