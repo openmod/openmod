da5dcfb944aa464bc75ed72780a1de83862bb304
Stephen White <iamsilk3495@gmail.com>
2021-04-18 15:13:15 -0300

    Added grantAdminsAllPerms config for Unturned admins

--- unturned/OpenMod.Unturned/Permissions/UnturnedAdminPermissionCheckProvider.cs
+++ unturned/OpenMod.Unturned/Permissions/UnturnedAdminPermissionCheckProvider.cs
@@ -1,14 +1,32 @@
-﻿using OpenMod.API.Prioritization;
-using OpenMod.Core.Permissions;
+﻿using Microsoft.Extensions.Configuration;
+using OpenMod.API.Permissions;
+using OpenMod.API.Prioritization;
+using OpenMod.Unturned.Configuration;
 using OpenMod.Unturned.Users;
+using System.Threading.Tasks;
 
 namespace OpenMod.Unturned.Permissions
 {
     [Priority(Priority = Priority.High)]
-    public class UnturnedAdminPermissionCheckProvider : AlwaysGrantPermissionCheckProvider
+    public class UnturnedAdminPermissionCheckProvider : IPermissionCheckProvider
     {
-        public UnturnedAdminPermissionCheckProvider() : base(actor => actor is UnturnedUser user && user.Player.SteamPlayer.isAdmin)
+        private readonly IConfiguration m_Configuration;
+
+        public UnturnedAdminPermissionCheckProvider(IOpenModUnturnedConfiguration configuration)
         {
+            m_Configuration = configuration.Configuration;
+        }
+
+        public bool SupportsActor(IPermissionActor actor)
+        {
+            return actor is UnturnedUser user && user.Player.SteamPlayer.isAdmin;
+        }
+
+        public Task<PermissionGrantResult> CheckPermissionAsync(IPermissionActor actor, string permission)
+        {
+            return Task.FromResult(m_Configuration.GetValue("grantAdminsAllPerms", defaultValue: true)
+                ? PermissionGrantResult.Grant
+                : PermissionGrantResult.Default);
         }
     }
 }
