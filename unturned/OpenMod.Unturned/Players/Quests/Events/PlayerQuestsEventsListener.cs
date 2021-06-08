ba76fe01ee72932f8b0a840444a994a7db578cca
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2021-06-08 10:24:31 +0200

    Added the PlayerQuests.onGroupChanged event to OpenMod

---
+++ unturned/OpenMod.Unturned/Players/Quests/Events/PlayerQuestsEventsListener.cs
@@ -0,0 +1,36 @@
+﻿extern alias JetBrainsAnnotations;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.Unturned.Events;
+using SDG.Unturned;
+using Steamworks;
+using System;
+
+namespace OpenMod.Unturned.Players.Quests.Events
+{
+    [UsedImplicitly]
+    internal abstract class PlayerQuestsEventsListener : UnturnedPlayerEventsListener
+    {
+        public PlayerQuestsEventsListener(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public override void Subscribe()
+        {
+            PlayerQuests.onGroupChanged += OnGroupChanged;
+        }
+
+        private void OnGroupChanged(PlayerQuests sender, CSteamID oldGroupID, EPlayerGroupRank oldGroupRank, CSteamID newGroupID, EPlayerGroupRank newGroupRank)
+        {
+            var player = GetUnturnedPlayer(sender.player)!;
+
+            var @event = new UnturnedPlayerGroupChangedEvent(player, oldGroupID, oldGroupRank, newGroupID, newGroupRank);
+
+            Emit(@event);
+        }
+
+        public override void Unsubscribe()
+        {
+            PlayerQuests.onGroupChanged -= OnGroupChanged;
+        }
+    }
+}
---
+++ unturned/OpenMod.Unturned/Players/Quests/Events/UnturnedPlayerGroupChangedEvent.cs
@@ -0,0 +1,24 @@
+﻿extern alias JetBrainsAnnotations;
+using OpenMod.Unturned.Events;
+using SDG.Unturned;
+using Steamworks;
+
+namespace OpenMod.Unturned.Players.Quests.Events
+{
+    public class UnturnedPlayerGroupChangedEvent : UnturnedPlayerEvent
+    {
+        public CSteamID OldGroupID { get; }
+        public EPlayerGroupRank OldGroupRank { get;  }
+
+        public CSteamID NewGroupID { get; }
+        public EPlayerGroupRank NewGroupRank { get; }
+
+        public UnturnedPlayerGroupChangedEvent(UnturnedPlayer player, CSteamID oldGroupID,
+            EPlayerGroupRank oldGroupRank, CSteamID newGroupID, EPlayerGroupRank newGroupRank) : base(player)
+        {
+            OldGroupID = oldGroupID;
+            OldGroupRank = oldGroupRank;
+            NewGroupID = newGroupID;
+        }
+    }
+}
