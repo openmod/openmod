dbe65cc503dd848c90a2c8912a6d8532555c87c4
Stephen White <iamsilk3495@gmail.com>
2021-06-10 11:19:01 -0300

    Add OpenMod host string localizer

---
+++ framework/OpenMod.API/Localization/IOpenModHostStringLocalizer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Localization;
+using OpenMod.API.Ioc;
+
+namespace OpenMod.API.Localization
+{
+    /// <summary>
+    /// The service used for localizing the OpenMod host's messages.
+    /// </summary>
+    /// <remarks>
+    /// This service is not used for localizing plugin messages.
+    /// </remarks>
+    [Service]
+    public interface IOpenModHostStringLocalizer : IStringLocalizer
+    {
+    }
+}
