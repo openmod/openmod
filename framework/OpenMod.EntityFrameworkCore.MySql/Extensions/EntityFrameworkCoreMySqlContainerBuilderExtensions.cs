d2035f7f0401af5d970f4ed463afe5a91215ffc1
Stephen White <iamsilk3495@gmail.com>
2021-06-10 19:46:56 -0300

    Added extension method needed for MySql db contexts

---
+++ framework/OpenMod.EntityFrameworkCore.MySql/Extensions/EntityFrameworkCoreMySqlContainerBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+
+namespace OpenMod.EntityFrameworkCore.MySql.Extensions
+{
+    public static class EntityFrameworkCoreMySqlContainerBuilderExtensions
+    {
+        public static ContainerBuilder AddEntityFrameworkCoreMySql(this ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<ConfigurationBasedConnectionStringAccessor>()
+                .As<ConfigurationBasedConnectionStringAccessor>()
+                .As<IConnectionStringAccessor>()
+                .OwnedByLifetimeScope()
+                .InstancePerDependency();
+
+            containerBuilder.RegisterType<PomeloMySqlConnectorResolver>()
+                .AsSelf()
+                .SingleInstance()
+                .AutoActivate();
+
+            return containerBuilder;
+        }
+    }
+}
