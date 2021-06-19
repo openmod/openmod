c72f119cbc3663c37711019ad7b24e23697ca111
Stephen White <iamsilk3495@gmail.com>
2021-06-19 16:28:48 -0300

    Re-added relational specific properties which proper documentation

--- framework/OpenMod.EntityFrameworkCore/OpenModDbContext.cs
+++ framework/OpenMod.EntityFrameworkCore/OpenModDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OpenMod.API.Plugins;
 using System;
+using System.Reflection;
 
 namespace OpenMod.EntityFrameworkCore
 {
@@ -16,6 +18,39 @@ namespace OpenMod.EntityFrameworkCore
             base(options)
         {
             m_ServiceProvider = serviceProvider;
+        /// <summary>
+        /// Gets the name of the migrations table for supporting providers.
+        /// </summary>
+        protected internal virtual string MigrationsTableName
+        {
+            get
+            {
+                var componentId = GetType().Assembly.GetCustomAttribute<PluginMetadataAttribute>().Id;
+                return "__" + componentId.Replace(".", "_") + "_MigrationsHistory".ToLower();
+            }
+        }
+
+        /// <summary>
+        /// Gets the prefix for the tables for supporting providers.
+        /// </summary>
+        protected internal virtual string? TablePrefix
+        {
+            get
+            {
+                var componentId = GetType().Assembly.GetCustomAttribute<PluginMetadataAttribute>()?.Id ??
+                                  throw new InvalidOperationException("Could not find plugin metadata");
+
+                return componentId.Replace(".", "_") + "_";
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the connection string used by supporting providers.
+        /// </summary>
+        protected internal virtual string GetConnectionStringName()
+        {
+            return typeof(TSelf).GetCustomAttribute<ConnectionStringAttribute>()?.Name ??
+                   ConnectionStrings.Default;
         }
     }
 }
