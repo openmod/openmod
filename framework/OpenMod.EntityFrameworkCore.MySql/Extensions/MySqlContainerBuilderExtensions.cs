b3ab03c771c8e6d05dea14d507169047fb2f7ff3
Stephen White <iamsilk3495@gmail.com>
2021-06-19 16:31:39 -0300

    Added AddMySqlDbContext extensions

---
+++ framework/OpenMod.EntityFrameworkCore.MySql/Extensions/MySqlContainerBuilderExtensions.cs
@@ -0,0 +1,74 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OpenMod.EntityFrameworkCore.Extensions;
+using OpenMod.EntityFrameworkCore.MySql.Configurator;
+using System;
+
+namespace OpenMod.EntityFrameworkCore.MySql.Extensions
+{
+    public static class MySqlContainerBuilderExtensions
+    {
+        public static ContainerBuilder AddMySqlDbContext<T>(this ContainerBuilder containerBuilder, string connectionString) where T : OpenModDbContext<T>
+        {
+            return AddMySqlDbContextInternal(containerBuilder, typeof(T), null, null);
+        }
+
+        public static ContainerBuilder AddMySqlDbContext<T>(this ContainerBuilder containerBuilder, ServiceLifetime serviceLifetime) where T : OpenModDbContext<T>
+        {
+            return AddMySqlDbContextInternal(containerBuilder, typeof(T), null, serviceLifetime);
+        }
+
+        public static ContainerBuilder AddMySqlDbContext<T>(this ContainerBuilder containerBuilder, Action<DbContextOptionsBuilder>? optionsBuilder) where T : OpenModDbContext<T>
+        {
+            return AddMySqlDbContextInternal(containerBuilder, typeof(T), optionsBuilder, null);
+        }
+
+        public static ContainerBuilder AddMySqlDbContext<T>(this ContainerBuilder containerBuilder, Action<DbContextOptionsBuilder>? optionsBuilder, ServiceLifetime serviceLifetime) where T : OpenModDbContext<T>
+        {
+            return AddMySqlDbContextInternal(containerBuilder, typeof(T), optionsBuilder, serviceLifetime);
+        }
+
+        public static ContainerBuilder AddMySqlDbContext(this ContainerBuilder containerBuilder, Type dbContextType)
+        {
+            return AddMySqlDbContextInternal(containerBuilder, dbContextType, null, null);
+        }
+
+        public static ContainerBuilder AddMySqlDbContext(this ContainerBuilder containerBuilder, Type dbContextType, ServiceLifetime serviceLifetime)
+        {
+            return AddMySqlDbContextInternal(containerBuilder, dbContextType, null, serviceLifetime);
+        }
+
+        public static ContainerBuilder AddMySqlDbContext(this ContainerBuilder containerBuilder, Type dbContextType, Action<DbContextOptionsBuilder>? optionsBuilder)
+        {
+            return AddMySqlDbContextInternal(containerBuilder, dbContextType, optionsBuilder, null);
+        }
+
+        public static ContainerBuilder AddMySqlDbContext(this ContainerBuilder containerBuilder, Type dbContextType, Action<DbContextOptionsBuilder>? optionsBuilder, ServiceLifetime serviceLifetime)
+        {
+            return AddMySqlDbContextInternal(containerBuilder, dbContextType, optionsBuilder, serviceLifetime);
+        }
+
+        internal static ContainerBuilder AddMySqlDbContextInternal(this ContainerBuilder containerBuilder,
+            Type dbContextType,
+            Action<DbContextOptionsBuilder>? optionsBuilder,
+            ServiceLifetime? serviceLifetime)
+        {
+            containerBuilder.RegisterType<ConfigurationBasedConnectionStringAccessor>()
+                .As<ConfigurationBasedConnectionStringAccessor>()
+                .As<IConnectionStringAccessor>()
+                .OwnedByLifetimeScope()
+                .InstancePerDependency();
+
+            containerBuilder.RegisterType<PomeloMySqlConnectorResolver>()
+                .AsSelf()
+                .SingleInstance()
+                .AutoActivate();
+
+            containerBuilder.AddDbContextInternal(dbContextType, optionsBuilder,
+                serviceLifetime ?? ServiceLifetime.Transient, new MySqlDbContextConfigurator());
+
+            return containerBuilder;
+        }
+    }
+}
