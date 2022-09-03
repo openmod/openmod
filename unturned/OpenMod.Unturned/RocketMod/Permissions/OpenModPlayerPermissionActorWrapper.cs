da5cb30dcbe5ad6087579b244e3c50c52c1913ec
Rúben Garcia <rube200@users.noreply.github.com>
2022-09-03 03:21:35 +0100

    Fix parse for OpenModPlayerPermissionActorWrapper

--- unturned/OpenMod.Unturned/RocketMod/Permissions/OpenModPlayerPermissionActorWrapper.cs
+++ unturned/OpenMod.Unturned/RocketMod/Permissions/OpenModPlayerPermissionActorWrapper.cs
@@ -1,5 +1,6 @@
 ﻿using OpenMod.API.Permissions;
 using OpenMod.Core.Users;
+using OpenMod.Unturned.RocketMod.Commands;
 using Rocket.API;
 
 namespace OpenMod.Unturned.RocketMod.Permissions
@@ -11,10 +12,29 @@ namespace OpenMod.Unturned.RocketMod.Permissions
         public OpenModPlayerPermissionActorWrapper(IRocketPlayer player)
         {
             Player = player;
-            Id = player.Id;
-            Type = KnownActorTypes.Player;
-            DisplayName = player.DisplayName;
-            FullActorName = $"rocketmodPlayer/{Id} ({DisplayName})";
+            switch (player)
+            {
+                case RconPlayer rcon:
+                    Id = rcon.CommandActor?.Id ?? "root";
+                    Type = rcon.CommandActor?.Type ?? KnownActorTypes.Rcon;
+                    DisplayName = rcon.CommandActor?.DisplayName ?? "Rcon";
+                    FullActorName = rcon.CommandActor?.FullActorName ?? $"rocketmodRcon/{Id} ({DisplayName})";
+                    break;
+
+                case ConsolePlayer:
+                    Id = player.Id;
+                    Type = KnownActorTypes.Console;
+                    DisplayName = player.DisplayName;
+                    FullActorName = $"rocketmodConsole/{Id} ({DisplayName})";
+                    break;
+
+                default:
+                    Id = player.Id;
+                    Type = KnownActorTypes.Player;
+                    DisplayName = player.DisplayName;
+                    FullActorName = $"rocketmodPlayer/{Id} ({DisplayName})";
+                    break;
+            }
         }
 
         public string Id { get; }
