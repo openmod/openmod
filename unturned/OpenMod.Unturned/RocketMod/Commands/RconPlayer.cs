82ba7f7eecb51bd431cd4a8af57d66a277f4b2d7
Rúben Garcia <rube200@users.noreply.github.com>
2022-09-03 03:20:22 +0100

    Pass ICommandActor to rcon

--- unturned/OpenMod.Unturned/RocketMod/Commands/RconPlayer.cs
+++ unturned/OpenMod.Unturned/RocketMod/Commands/RconPlayer.cs
@@ -1,7 +1,18 @@
-﻿using Rocket.API;
+﻿using OpenMod.API.Commands;
+using Rocket.API;
 
 namespace OpenMod.Unturned.RocketMod.Commands
 {
     public class RconPlayer : ConsolePlayer
-    { }
+    {
+        public readonly ICommandActor? CommandActor;
+
+        public RconPlayer()
+        { }
+
+        public RconPlayer(ICommandActor actor)
+        {
+            CommandActor = actor;
+        }
+    }
 }
