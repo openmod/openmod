6a49adcb54eab57249ff39b03add12bf26dfab92
Rúben Garcia <rube200@users.noreply.github.com>
2022-09-19 22:20:00 +0100

    Rocketmod rcon new line changes

--- unturned/OpenMod.Unturned/RocketMod/Rcon/RocketModRconPacket.cs
+++ unturned/OpenMod.Unturned/RocketMod/Rcon/RocketModRconPacket.cs
@@ -7,21 +7,29 @@ namespace OpenMod.Unturned.RocketMod.Rcon
     public class RocketModRconPacket
     {
         public string? Body { get; set; }
+        public NewLineType NewLineType { get; set; }
 
         public static async Task<RocketModRconPacket> ReadFromStreamAsync(Stream stream)
         {
             var buffer = new byte[stream.Length];
-            await stream.ReadAsync(buffer, 0, buffer.Length);
+            _ = await stream.ReadAsync(buffer, 0, buffer.Length);
 
             return new RocketModRconPacket
             {
-                Body = Encoding.UTF8.GetString(buffer, 0, buffer.Length - 2)
+                Body = Encoding.UTF8.GetString(buffer)
             };
         }
 
         public byte[] Serialize()
         {
-            return Encoding.UTF8.GetBytes(Body + "\r\n");
+            var endLine = NewLineType switch
+            {
+                NewLineType.Windows => "\r\n",
+                NewLineType.Linux => "\n",
+                NewLineType.Mac => "\r",
+                _ => "\n"
+            };
+            return Encoding.UTF8.GetBytes(Body + endLine);
         }
     }
 }
