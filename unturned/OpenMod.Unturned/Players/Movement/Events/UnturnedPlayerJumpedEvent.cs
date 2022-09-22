6ffdb4ef6f1f8ea17d6ee036755b0978094b2afc
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2022-09-22 23:07:03 +0700

    Add UnturnedPlayerJumped event

---
+++ unturned/OpenMod.Unturned/Players/Movement/Events/UnturnedPlayerJumpedEvent.cs
@@ -0,0 +1,11 @@
+﻿using OpenMod.Unturned.Events;
+
+namespace OpenMod.Unturned.Players.Movement.Events
+{
+    public class UnturnedPlayerJumpedEvent : UnturnedPlayerEvent
+    {
+        public UnturnedPlayerJumpedEvent(UnturnedPlayer player) : base(player)
+        {
+        }
+    }
+}
