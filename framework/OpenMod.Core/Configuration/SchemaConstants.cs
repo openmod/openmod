493a4c71703ac1bb609d97856b06e66e6fbfd3b0
Andrii Vitiv <cyberandrii@gmail.com>
2022-10-02 18:34:04 +0300

    Add schema for openmod.roles.yaml

---
+++ framework/OpenMod.Core/Configuration/SchemaConstants.cs
@@ -0,0 +1,9 @@
+namespace OpenMod.Core.Configuration
+{
+    internal static class SchemaConstants
+    {
+        private const string c_SchemasDirectory = ".schemas";
+
+        public const string c_RolesSchemaPath = c_SchemasDirectory + "/openmod.roles.yaml.schema.json";
+    }
+}
---
+++ framework/OpenMod.Core/Permissions/PermissionsUtils.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MoreLinq;
+using OpenMod.API;
+using OpenMod.API.Commands;
+using OpenMod.API.Permissions;
+using OpenMod.API.Plugins;
+
+namespace OpenMod.Core.Permissions
+{
+    internal static class PermissionsUtils
+    {
+        public static async Task<ISet<string>> GetAllPossiblePermissionsAsync(IServiceProvider serviceProvider)
+        {
+            var permissionRegistry = serviceProvider.GetRequiredService<IPermissionRegistry>();
+            var openModHost = serviceProvider.GetRequiredService<IOpenModHost>();
+            var pluginActivator = serviceProvider.GetRequiredService<IPluginActivator>();
+            var commandStore = serviceProvider.GetRequiredService<ICommandStore>();
+            var commandPermissionBuilder = serviceProvider.GetRequiredService<ICommandPermissionBuilder>();
+
+            var openModComponents = new IOpenModComponent[] { openModHost }
+                .Concat(pluginActivator.ActivatedPlugins)
+                .ToArray();
+
+            var permissionsOfAllCommands = await GetPermissionsOfAllCommandsAsync(commandStore, commandPermissionBuilder);
+            var permissionsOfAllComponents = GetPermissionsOfAllComponents(permissionRegistry, openModComponents);
+
+            var allPermissions = permissionsOfAllComponents
+                .Union(permissionsOfAllCommands)
+                .ToArray();
+
+            return allPermissions
+                .Union(allPermissions.Select(p => '!' + p))
+                .ToHashSet();
+        }
+
+        private static async Task<ISet<string>> GetPermissionsOfAllCommandsAsync(
+            ICommandStore commandStore,
+            ICommandPermissionBuilder commandPermissionBuilder)
+        {
+            var commands = await commandStore.GetCommandsAsync();
+
+            return commands
+                .SelectMany(c =>
+                {
+                    var permission = commandPermissionBuilder.GetPermission(c, commands);
+
+                    var permissionRegistrations = c.PermissionRegistrations?
+                        .Select(r => permission + '.' + r.Permission) ?? Enumerable.Empty<string>();
+
+                    // ReSharper disable once InvokeAsExtensionMethod
+                    return MoreEnumerable.Append(permissionRegistrations, permission);
+                })
+                .SelectMany(DefaultPermissionCheckProvider.BuildPermissionTree)
+                .ToHashSet();
+        }
+
+        private static ISet<string> GetPermissionsOfAllComponents(
+            IPermissionRegistry permissionRegistry,
+            IReadOnlyCollection<IOpenModComponent> openModComponents)
+        {
+            return openModComponents
+                .SelectMany(c => permissionRegistry
+                    .GetPermissions(c)
+                    .Select(r => c.OpenModComponentId + '.' + r.Permission))
+                .SelectMany(DefaultPermissionCheckProvider.BuildPermissionTree)
+                .ToHashSet();
+        }
+    }
+}
