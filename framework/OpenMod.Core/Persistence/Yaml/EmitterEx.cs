4efb890521c6f7a7cd36330ffb0e07c17ea619d7
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2023-02-03 21:14:35 +0700

    Create ex version of emitter and value serializer

---
+++ framework/OpenMod.Core/Persistence/Yaml/EmitterEx.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+
+namespace OpenMod.Core.Persistence.Yaml
+{
+    /// <summary>
+    /// Emits YAML streams.
+    /// Ex: Caches scalar data for optimization
+    /// </summary>
+    internal sealed class EmitterEx : IEmitter
+    {
+        private delegate void AnalyzeScalar(Emitter emitter, Scalar scalar);
+        private delegate void AnalyzeTag(Emitter emitter, TagName tagName);
+        private delegate void StateMachine(Emitter emitter, ParsingEvent parsingEvent);
+        private delegate bool NeedsMoreEvents(Emitter emitter);
+
+        private static readonly ConcurrentDictionary<string, ScalarData> s_CachedScalarDatas;
+
+        private static readonly AccessTools.FieldRef<object, object> s_ScalarDataFieldRef;
+        private static readonly AccessTools.FieldRef<object, bool>[] s_ScalarDataBooleanFieldRefs;
+        private static readonly AccessTools.FieldRef<object, string> s_ScalarDataValueFieldRef;
+
+        private static readonly AccessTools.FieldRef<object, object> s_TagDataFieldRef;
+        private static readonly AccessTools.FieldRef<object, string>[] s_TagDataStringFieldRefs;
+
+        private static readonly AccessTools.FieldRef<object, Queue<ParsingEvent>> s_EventsFieldRef;
+
+        private static readonly StateMachine s_StateMachineMethod;
+        private static readonly AnalyzeTag s_AnalyzeTagMethod;
+        private static readonly NeedsMoreEvents s_NeedsMoreEventsMethod;
+        private static readonly AnalyzeScalar s_AnalyzeScalarMethod;
+
+        static EmitterEx()
+        {
+            s_CachedScalarDatas = new();
+
+            var emitterType = typeof(Emitter);
+            const BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.NonPublic;
+
+            var scalarDataField = emitterType.GetField("scalarData", bindingAttr);
+            s_ScalarDataFieldRef = AccessTools.FieldRefAccess<object>(emitterType, scalarDataField.Name);
+
+            var fields = scalarDataField.FieldType.GetFields();
+            s_ScalarDataBooleanFieldRefs = fields
+                .Where(x => x.FieldType == typeof(bool))
+                .Select(x => AccessTools.FieldRefAccess<bool>(scalarDataField.FieldType, x.Name))
+                .ToArray();
+
+            s_ScalarDataValueFieldRef
+                = AccessTools.FieldRefAccess<string>(scalarDataField.FieldType, fields.Single(x => x.FieldType == typeof(string)).Name);
+
+            var tagDataField = emitterType.GetField("tagData", bindingAttr);
+            s_TagDataFieldRef = AccessTools.FieldRefAccess<object>(emitterType, tagDataField.Name);
+
+            s_TagDataStringFieldRefs = tagDataField.FieldType.GetFields()
+                .Select(x => AccessTools.FieldRefAccess<string>(tagDataField.FieldType, x.Name))
+                .ToArray();
+
+            s_EventsFieldRef = AccessTools.FieldRefAccess<Queue<ParsingEvent>>(emitterType, "events");
+
+            s_StateMachineMethod = AccessTools.MethodDelegate<StateMachine>(
+            emitterType.GetMethod("StateMachine", bindingAttr), null, false);
+
+            s_AnalyzeTagMethod = AccessTools.MethodDelegate<AnalyzeTag>(
+            emitterType.GetMethod("AnalyzeTag", bindingAttr), null, false);
+
+            s_NeedsMoreEventsMethod = AccessTools.MethodDelegate<NeedsMoreEvents>(
+            emitterType.GetMethod(name: "NeedMoreEvents", bindingAttr), null, false);
+
+            s_AnalyzeScalarMethod = AccessTools.MethodDelegate<AnalyzeScalar>(
+            emitterType.GetMethod(name: "AnalyzeScalar", bindingAttr), null, false);
+        }
+
+        private readonly Emitter m_Emitter;
+        private readonly Queue<ParsingEvent> m_Events;
+        private readonly object m_TagDataValue;
+        private readonly object m_ScalarDataValue;
+
+        public EmitterEx(TextWriter output)
+        {
+            m_Emitter = new Emitter(output);
+
+            m_ScalarDataValue = s_ScalarDataFieldRef(m_Emitter);
+            m_TagDataValue = s_TagDataFieldRef(m_Emitter);
+            m_Events = s_EventsFieldRef(m_Emitter);
+        }
+
+        public void Emit(ParsingEvent @event)
+        {
+            m_Events.Enqueue(@event);
+
+            while (!s_NeedsMoreEventsMethod(m_Emitter))
+            {
+                var current = m_Events.Peek();
+                try
+                {
+                    ClearTagData();
+
+                    if (current is NodeEvent nodeEvent)
+                    {
+                        if (current is Scalar scalar)
+                        {
+                            AnalyzeScalarEx(scalar);
+                        }
+
+                        if (!nodeEvent.Tag.IsEmpty && nodeEvent.IsCanonical)
+                        {
+                            s_AnalyzeTagMethod(m_Emitter, nodeEvent.Tag);
+                        }
+                    }
+
+                    s_StateMachineMethod(m_Emitter, current);
+                }
+                finally
+                {
+                    m_Events.Dequeue();
+                }
+            }
+        }
+
+        private void ClearTagData()
+        {
+            foreach (var field in s_TagDataStringFieldRefs)
+            {
+                ref var data = ref field(m_TagDataValue);
+                data = null;
+            }
+        }
+
+        private void AnalyzeScalarEx(Scalar scalar)
+        {
+            if (scalar.Value.Length == 0)
+            {
+                s_AnalyzeScalarMethod(m_Emitter, scalar);
+                return;
+            }
+
+            if (s_CachedScalarDatas.TryGetValue(scalar.Value, out var data))
+            {
+                SetScalarData(m_ScalarDataValue, data);
+                return;
+            }
+
+            s_AnalyzeScalarMethod(m_Emitter, scalar);
+            s_CachedScalarDatas.TryAdd(scalar.Value, CopyScalarData(m_ScalarDataValue));
+        }
+
+        private static ScalarData CopyScalarData(object scalarData)
+        {
+            var booleans = new bool[6];
+            var value = s_ScalarDataValueFieldRef(scalarData);
+            for (var i = 0; i < s_ScalarDataBooleanFieldRefs.Length; i++)
+            {
+                var field = s_ScalarDataBooleanFieldRefs[i];
+                booleans[i] = field(scalarData);
+            }
+
+            return new(value, booleans);
+        }
+
+        private static void SetScalarData(object oldData, ScalarData newData)
+        {
+            ref var oldValue = ref s_ScalarDataValueFieldRef(oldData);
+            oldValue = newData.Value;
+
+            for (var i = 0; i < s_ScalarDataBooleanFieldRefs.Length; i++)
+            {
+                var field = s_ScalarDataBooleanFieldRefs[i];
+
+                ref var oldBool = ref field(oldData);
+                oldBool = newData.Booleans[i];
+            }
+        }
+
+        private sealed class ScalarData
+        {
+            public readonly string Value;
+            public readonly bool[] Booleans;
+
+            public ScalarData(string value, bool[] booleans)
+            {
+                Value = value;
+                Booleans = booleans;
+            }
+        }
+    }
+}
---
+++ framework/OpenMod.Core/Persistence/Yaml/ValueSerializerEx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using MoreLinq;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.Converters;
+using YamlDotNet.Serialization.EventEmitters;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization.ObjectGraphTraversalStrategies;
+using YamlDotNet.Serialization.ObjectGraphVisitors;
+using YamlDotNet.Serialization.TypeInspectors;
+using YamlDotNet.Serialization.TypeResolvers;
+using ITypeResolver = YamlDotNet.Serialization.ITypeResolver;
+
+[assembly: InternalsVisibleTo("OpenMod.Core.Benchmark")]
+
+namespace OpenMod.Core.Persistence.Yaml
+{
+    internal sealed class ValueSerializerEx : IValueSerializer
+    {
+        private static readonly ITypeResolver s_TypeResolver = new DynamicTypeResolver();
+
+        private readonly IObjectGraphTraversalStrategy m_TraversalStrategy;
+        private readonly IEventEmitter m_EventEmitter;
+        private readonly IYamlTypeConverter[] m_TypeConverters;
+        private readonly Func<EmissionPhaseObjectGraphVisitorArgs, IObjectGraphVisitor<IEmitter>>[] m_EmissionPhaseObjectGraphVisitorFactory;
+        private readonly IObjectGraphVisitor<IEmitter> m_InnerObjectGraphVisitor;
+
+        public ValueSerializerEx(INamingConvention namingConvention, bool ignoreFields, bool includeNonPublicProperties)
+        {
+            var typeInspector = BuildTypeInspector(namingConvention, ignoreFields, includeNonPublicProperties);
+            m_TypeConverters = BuildTypeConverters();
+            m_EventEmitter = BuildEventEmitter();
+            m_EmissionPhaseObjectGraphVisitorFactory = BuildEmissionPhaseObjectGraphVisitorFactory();
+            m_TraversalStrategy = new FullObjectGraphTraversalStrategy(typeInspector, s_TypeResolver, 50, namingConvention);
+            m_InnerObjectGraphVisitor = new EmittingObjectGraphVisitor(m_EventEmitter);
+        }
+
+        private ITypeInspector BuildTypeInspector(INamingConvention namingConvention, bool ignoreFields, bool includeNonPublicProperties)
+        {
+            ITypeInspector innerInspector = new ReadablePropertiesTypeInspector(s_TypeResolver, includeNonPublicProperties);
+            if (!ignoreFields)
+            {
+                innerInspector = new CompositeTypeInspector(new ReadableFieldsTypeInspector(s_TypeResolver), innerInspector);
+            }
+
+            // chain (reversed)
+            return new Func<ITypeInspector, ITypeInspector>[]
+            {
+                (inner) => new YamlAttributeOverridesInspector(inner, new()),
+                (inner) => new YamlAttributesTypeInspector(inner),
+                (inner) => namingConvention is NullNamingConvention ? inner : new NamingConventionTypeInspector(inner, namingConvention),
+                (inner) => new CachedTypeInspector(inner),
+            }.Aggregate(innerInspector, (inner, factory) => factory(inner));
+        }
+
+        private IYamlTypeConverter[] BuildTypeConverters()
+        {
+            return new IYamlTypeConverter[]
+            {
+                new GuidConverter(jsonCompatible: false),
+                new SystemTypeConverter(),
+            };
+        }
+
+        private IEventEmitter BuildEventEmitter()
+        {
+            IEventEmitter innerEmitter = new WriterEventEmitter();
+            return new Func<IEventEmitter, IEventEmitter>[]
+            {
+                (inner) => new TypeAssigningEventEmitter(inner, false, new Dictionary<Type, TagName>()),
+            }.Aggregate(innerEmitter, (inner, factory) => factory(inner));
+        }
+
+        private Func<EmissionPhaseObjectGraphVisitorArgs, IObjectGraphVisitor<IEmitter>>[] BuildEmissionPhaseObjectGraphVisitorFactory()
+        {
+            return new Func<EmissionPhaseObjectGraphVisitorArgs, IObjectGraphVisitor<IEmitter>>[]
+            {
+                (args) => new CommentsObjectGraphVisitor(args.innerVisitor),
+                (args) => new DefaultValuesObjectGraphVisitor(DefaultValuesHandling.Preserve, args.innerVisitor),
+                (args) => new CustomSerializationObjectGraphVisitor(args.innerVisitor, args.typeConverters, args.objectSerializer),
+            };
+        }
+
+        public void SerializeValue(IEmitter emitter, object? value, Type? type)
+        {
+            var type2 = type ?? ((value != null) ? value.GetType() : typeof(object));
+            var staticType = type ?? typeof(object);
+            var graph = new ObjectDescriptor(value, type2, staticType);
+
+            void NestedObjectSerializer(object? v, Type? t)
+            {
+                SerializeValue(emitter, v, t);
+            }
+            ObjectSerializer objectSerializer = NestedObjectSerializer;
+
+            var visitor = m_EmissionPhaseObjectGraphVisitorFactory
+                .Aggregate(m_InnerObjectGraphVisitor, (inner, factory)
+                    => factory(new EmissionPhaseObjectGraphVisitorArgs(inner, m_TypeConverters, objectSerializer)));
+
+            m_TraversalStrategy.Traverse(graph, visitor, emitter);
+        }
+
+        private readonly struct EmissionPhaseObjectGraphVisitorArgs
+        {
+            public readonly IObjectGraphVisitor<IEmitter> innerVisitor;
+            public readonly IYamlTypeConverter[] typeConverters;
+            public readonly ObjectSerializer objectSerializer;
+
+            public EmissionPhaseObjectGraphVisitorArgs(IObjectGraphVisitor<IEmitter> innerVisitor,
+                IYamlTypeConverter[] typeConverters, ObjectSerializer objectSerializer)
+            {
+                this.innerVisitor = innerVisitor;
+                this.typeConverters = typeConverters;
+                this.objectSerializer = objectSerializer;
+            }
+        }
+    }
+}
