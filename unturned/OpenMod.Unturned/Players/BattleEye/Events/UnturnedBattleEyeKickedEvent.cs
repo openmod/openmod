3fde33f355d9be31c7da58679072fd2d84c11d77
Jakub Jurczyk <pandetthe@gmail.com>
2023-04-11 20:55:25 +0200

    Add BattleEyeKickedEvent and PlayerRejectingEvent

---
+++ unturned/OpenMod.Unturned/Players/BattleEye/Events/UnturnedBattleEyeKickedEvent.cs
@@ -0,0 +1,14 @@
+﻿using OpenMod.Unturned.Events;
+
+namespace OpenMod.Unturned.Players.BattleEye.Events
+{
+    public class UnturnedBattleEyeKickedEvent : UnturnedPlayerEvent
+    {
+        public string Reason { get; set; }
+
+        public UnturnedBattleEyeKickedEvent(UnturnedPlayer player, string reason) : base(player)
+        {
+            Reason = reason;
+        }
+    }
+}
