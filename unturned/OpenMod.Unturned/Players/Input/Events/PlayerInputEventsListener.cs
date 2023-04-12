0911b1e838fc80204e5db1647b2e943d172022ee
sunnamed434 <cetnikletsplay@gmail.com>
2023-04-12 15:02:08 +0300

    Add UnturnedPlayerInputtingEvent

--- unturned/OpenMod.Unturned/Players/Input/Events/PlayerInputEventsListener.cs
+++ unturned/OpenMod.Unturned/Players/Input/Events/PlayerInputEventsListener.cs
@@ -1,13 +1,14 @@
 ﻿extern alias JetBrainsAnnotations;
 using System;
-
+using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrainsAnnotations::JetBrains.Annotations;
 using OpenMod.Unturned.Events;
-
 using SDG.Unturned;
 
 namespace OpenMod.Unturned.Players.Input.Events
 {
-    [JetBrainsAnnotations::JetBrains.Annotations.UsedImplicitlyAttribute]
+    [UsedImplicitly]
     internal class PlayerInputEventsListener : UnturnedEventsListener
     {
         public PlayerInputEventsListener(IServiceProvider serviceProvider) : base(serviceProvider)
@@ -17,12 +18,15 @@ namespace OpenMod.Unturned.Players.Input.Events
         public override void Subscribe()
         {
             PlayerInput.onPluginKeyTick += OnPluginKeyTick;
+            OnPlayerInputted += Events_OnPlayerInputted;
         }
 
         public override void Unsubscribe()
         {
             PlayerInput.onPluginKeyTick -= OnPluginKeyTick;
+            OnPlayerInputted -= Events_OnPlayerInputted;
         }
+
 
         private readonly bool[] m_LastInputs = new bool[ControlsSettings.NUM_PLUGIN_KEYS];
 
@@ -44,6 +48,34 @@ namespace OpenMod.Unturned.Players.Input.Events
             var @event = new UnturnedPlayerPluginKeyStateChangedEvent(player, key, state);
 
             Emit(@event);
+        }
+
+        private void Events_OnPlayerInputted(PlayerInput input, ref InputInfo? inputinfo, bool doOcclusionCheck, ERaycastInfoUsage usage)
+        {
+            var player = GetUnturnedPlayer(input.player)!;
+            var @event = new UnturnedPlayerInputtingEvent(player, inputinfo, doOcclusionCheck, usage);
+
+            Emit(@event);
+
+            inputinfo = @event.InputInfo;
+        }
+
+        private delegate void PlayerInputted(PlayerInput input, ref InputInfo? inputInfo, bool doOcclusionCheck, ERaycastInfoUsage usage);
+
+        private static event PlayerInputted? OnPlayerInputted;
+
+        [UsedImplicitly]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [HarmonyPatch]
+        public static class Patches
+        {
+            [UsedImplicitly]
+            [HarmonyPatch(typeof(PlayerInput), nameof(PlayerInput.getInput), typeof(bool), typeof(ERaycastInfoUsage))]
+            [HarmonyPostfix]
+            public static void PostGetInput(PlayerInput __instance, ref InputInfo __result, bool doOcclusionCheck, ERaycastInfoUsage usage)
+            {
+                OnPlayerInputted?.Invoke(__instance, ref __result!, doOcclusionCheck, usage);
+            }
         }
     }
 }
---
+++ unturned/OpenMod.Unturned/Players/Input/Events/UnturnedPlayerInputtingEvent.cs
@@ -0,0 +1,23 @@
+﻿using OpenMod.Unturned.Events;
+using SDG.Unturned;
+
+namespace OpenMod.Unturned.Players.Input.Events
+{
+    public class UnturnedPlayerInputtingEvent : UnturnedPlayerEvent
+    {
+        public UnturnedPlayerInputtingEvent(UnturnedPlayer player, InputInfo? inputInfo, bool doOcclusionCheck, ERaycastInfoUsage usage) : base(player)
+        {
+            InputInfo = inputInfo;
+            DoOcclusionCheck = doOcclusionCheck;
+            Usage = usage;
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="InputInfo"/>.
+        /// <remarks>Changes to the model is going to be applied after the event.</remarks>
+        /// </summary>
+        public InputInfo? InputInfo { get; set; }
+        public bool DoOcclusionCheck { get; set; }
+        public ERaycastInfoUsage Usage { get; }
+    }
+}
