645af6143eee70d111fd878d6183c1f13a8716ea
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2023-10-16 07:44:19 +0700

    Fix mono `Socket.ReceiveAsync` bug

---
+++ unturned/OpenMod.Unturned/Mono/Patch_SocketTaskExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace OpenMod.Unturned.Mono;
+[HarmonyPatch]
+internal static class Patch_SocketTaskExtensions
+{
+    [HarmonyPatch(typeof(SocketTaskExtensions), nameof(SocketTaskExtensions.ReceiveAsync),
+    new[] { typeof(Socket), typeof(Memory<byte>), typeof(SocketFlags), typeof(CancellationToken) })]
+    [HarmonyPrefix]
+    public static bool SocketTaskExtensionsReceiveAsync(Socket socket, Memory<byte> memory, SocketFlags socketFlags, CancellationToken cancellationToken,
+        out ValueTask<int> __result)
+    {
+        if (MemoryMarshal.TryGetArray((ReadOnlyMemory<byte>)memory, out var segment))
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>(socket);
+            socket.BeginReceive(segment.Array, 0, segment.Array.Length, socketFlags, iar =>
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                var taskCompletionSource2 = (TaskCompletionSource<int>)iar.AsyncState;
+                var socket2 = (Socket)taskCompletionSource2.Task.AsyncState;
+                try
+                {
+                    taskCompletionSource2.TrySetResult(socket2.EndReceive(iar));
+                }
+                catch (Exception ex)
+                {
+                    taskCompletionSource2.TrySetException(ex);
+                }
+            }, taskCompletionSource);
+
+            cancellationToken.ThrowIfCancellationRequested();
+            __result = new ValueTask<int>(taskCompletionSource.Task);
+        }
+        else
+        {
+            __result = new(0);
+        }
+
+        return false;
+    }
+}
