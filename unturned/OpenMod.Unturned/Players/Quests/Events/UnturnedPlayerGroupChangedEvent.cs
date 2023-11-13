e1d51ae9aeab4e6415ee451a09154f43d8cf61bf
Rúben Garcia <rube200@users.noreply.github.com>
2023-11-13 22:43:00 +0000

    Update UnturnedPlayerGroupChangedEvent.cs

--- unturned/OpenMod.Unturned/Players/Quests/Events/UnturnedPlayerGroupChangedEvent.cs
+++ unturned/OpenMod.Unturned/Players/Quests/Events/UnturnedPlayerGroupChangedEvent.cs
@@ -4,7 +4,7 @@ using Steamworks;
 
 namespace OpenMod.Unturned.Players.Quests.Events
 {
-    [Obsolete($"Use {nameof(UnturnedPlayerGroupOrRankChangedEvent)} or {nameof(UnturnedPlayerGroupIdChangedEvent)}")]
+    [Obsolete($"Use {nameof(UnturnedPlayerGroupIdChangedEvent)} or {nameof(UnturnedPlayerGroupOrRankChangedEvent)}")]
     public class UnturnedPlayerGroupChangedEvent : UnturnedPlayerGroupOrRankChangedEvent
     {
         public UnturnedPlayerGroupChangedEvent(UnturnedPlayer player, CSteamID oldGroupId, EPlayerGroupRank oldGroupRank, CSteamID newGroupId, EPlayerGroupRank newGroupRank) : base(player, oldGroupId, oldGroupRank, newGroupId, newGroupRank)
