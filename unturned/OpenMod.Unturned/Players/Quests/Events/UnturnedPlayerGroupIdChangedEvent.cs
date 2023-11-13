32c4385a1f6a74f97dda862318d7fda8078d0206
Rúben Garcia <rube200@users.noreply.github.com>
2023-11-13 22:42:24 +0000

    Create UnturnedPlayerGroupIdChanged

---
+++ unturned/OpenMod.Unturned/Players/Quests/Events/UnturnedPlayerGroupIdChangedEvent.cs
@@ -0,0 +1,18 @@
+﻿using OpenMod.Unturned.Events;
+using Steamworks;
+
+namespace OpenMod.Unturned.Players.Quests.Events;
+
+public class UnturnedPlayerGroupIdChangedEvent : UnturnedPlayerEvent
+{
+    public CSteamID OldGroupId { get; }
+
+    public CSteamID NewGroupId { get; }
+
+    public UnturnedPlayerGroupIdChangedEvent(UnturnedPlayer player, CSteamID oldGroupId,
+        CSteamID newGroupId) : base(player)
+    {
+        OldGroupId = oldGroupId;
+        NewGroupId = newGroupId;
+    }
+}
