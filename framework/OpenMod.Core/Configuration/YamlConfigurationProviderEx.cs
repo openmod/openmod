75cf4297f61218a12201764114485c98d6f4647e
rube200 <rube200@users.noreply.github.com>
2024-06-07 00:35:30 +0100

    Fix ConfigProvider direct deserialization not working
    Move from direct(Dict<string, string>) to indirect(Dict<string, object>) and then proc

--- framework/OpenMod.Core/Configuration/YamlConfigurationProviderEx.cs
+++ framework/OpenMod.Core/Configuration/YamlConfigurationProviderEx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
+using System.Text;
 using Microsoft.Extensions.Configuration;
 using OpenMod.API;
 using OpenMod.Common.Helpers;
@@ -13,31 +15,105 @@ namespace OpenMod.Core.Configuration
     /// Ex: Supports variables.
     /// </summary>
     [OpenModInternal]
-    public class YamlConfigurationProviderEx : FileConfigurationProvider
+    public class YamlConfigurationProviderEx(YamlConfigurationSourceEx source) : FileConfigurationProvider(source)
     {
-        public YamlConfigurationProviderEx(YamlConfigurationSourceEx source) : base(source)
-        { }
-
         public override void Load(Stream stream)
         {
-            using var reader = new StreamReader(stream, detectEncodingFromByteOrderMarks: true);
-            var yaml = reader.ReadToEnd();
-
-            PreProcessYaml(ref yaml);
-
-            using var outStream = new MemoryStream();
-            using var writer = new StreamWriter(outStream, reader.CurrentEncoding);
-            writer.Write(yaml);
-            writer.Flush();
-            outStream.Seek(0, SeekOrigin.Begin);
+            var binData = stream.ReadAllBytes();
+            var decodedData = UTF8Encoding.UTF8.GetString(binData);
+            PreProcessYaml(ref decodedData);
+            binData = UTF8Encoding.UTF8.GetBytes(decodedData);
 
             try
             {
-                Data = YamlSerializer.Deserialize<IDictionary<string, string?>>(outStream.ReadAllBytes());
+                var nodes = YamlSerializer.Deserialize<IDictionary<string, object>>(binData);
+                var processedNode = GetKeyValuesFromNode(nodes);
+                Data = processedNode.ToImmutableSortedDictionary();
             }
             catch (YamlSerializerException e)
             {
                 throw new FormatException($"Could not parse the YAML file: {e.Message}.", e);
+            }
+        }
+
+        private IEnumerable<KeyValuePair<string, string?>> GetKeyValuesFromNode(IEnumerable<KeyValuePair<string, object>> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                var nodeData = GetKeyValuesFromNode(node.Value);
+                foreach (var item in nodeData)
+                {
+                    yield return KeyValuePair.Create($"{node.Key}:{item.Key}", item.Value);
+                }
+            }
+        }
+
+        private IEnumerable<KeyValuePair<string, string?>> GetKeyValuesFromNode(object node)
+        {
+            switch (node)
+            {
+                case IDictionary<object, object> dict:
+                    foreach (var item in GetKeyValuesFromNode(dict))
+                    {
+                        yield return item;
+                    }
+                    break;
+
+                case IList<object> list:
+                    foreach (var item in GetKeyValuesFromNode(list))
+                    {
+                        yield return item;
+                    }
+                    break;
+
+                default:
+                    var v = node.ToString();
+                    //YamlDotNet set all bool to lower case
+                    if (node is bool)
+                    {
+                        v = v.ToLower();
+                    }
+
+                    yield return KeyValuePair.Create<string, string?>(string.Empty, v);
+                    break;
+            }
+        }
+
+        private IEnumerable<KeyValuePair<string, string?>> GetKeyValuesFromNode(IDictionary<object, object> node)
+        {
+            foreach (var nodeItem in node)
+            {
+                var itemData = GetKeyValuesFromNode(nodeItem.Value);
+                foreach (var entry in itemData)
+                {
+                    var key = new StringBuilder(nodeItem.Key.ToString());
+                    if (!string.IsNullOrEmpty(entry.Key))
+                    {
+                        key.Append(":");
+                        key.Append(entry.Key);
+                    }
+
+                    yield return KeyValuePair.Create(key.ToString(), entry.Value);
+                }
+            }
+        }
+
+        private IEnumerable<KeyValuePair<string, string?>> GetKeyValuesFromNode(IList<object> node)
+        {
+            for (int i = 0; i < node.Count; i++)
+            {
+                var item = node[i];
+                var itemData = GetKeyValuesFromNode(item);
+                foreach (var entry in itemData)
+                {
+                    var key = new StringBuilder(i.ToString());
+                    if (!string.IsNullOrEmpty(entry.Key))
+                    {
+                        key.Append(":");
+                        key.Append(entry.Key);
+                    }
+                    yield return KeyValuePair.Create(key.ToString(), entry.Value);
+                }
             }
         }
 
